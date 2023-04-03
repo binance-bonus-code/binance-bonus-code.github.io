2616d6ffb76941e871fa0709bd42161ac4c44c77
binance-bonus-code <105982387+binance-bonus-code@users.noreply.github.com>
2023-04-03 10:04:34 +0800

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -2,7 +2,7 @@
   "user strict";
   // Preloader Js
   $(window).on('load', function () {
-    $('.overlayer').fadeOut(1);
+    $('.overlayer000').fadeOut(1000);
     var img = $('.bg_img');
     img.css('background-image', function () {
       var bg = ('url(' + $(this).data('img') + ')');
